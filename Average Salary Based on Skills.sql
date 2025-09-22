be114870551364cd1ae4d24cfb66ca853ca39561
Kushan Pathak <121530935+kushanpathak@users.noreply.github.com>
2025-09-22 22:22:30 +0530

    Update Average Salary Based on Skills.sql

--- Average Salary Based on Skills.sql
+++ Average Salary Based on Skills.sql
@@ -19,7 +19,7 @@ ORDER BY
 LIMIT 25;
 
 /*
-Here's a breakdown of the results for top paying skills for Data Analysts:
+The breakdown of the results for top paying skills for Data Analysts:
 - High Demand for Big Data & ML Skills: Top salaries are commanded by analysts skilled in big data technologies (PySpark, Couchbase), machine learning tools (DataRobot, Jupyter), and Python libraries (Pandas, NumPy), reflecting the industry's high valuation of data processing and predictive modeling capabilities.
 - Software Development & Deployment Proficiency: Knowledge in development and deployment tools (GitLab, Kubernetes, Airflow) indicates a lucrative crossover between data analysis and engineering, with a premium on skills that facilitate automation and efficient data pipeline management.
 - Cloud Computing Expertise: Familiarity with cloud and data engineering tools (Elasticsearch, Databricks, GCP) underscores the growing importance of cloud-based analytics environments, suggesting that cloud proficiency significantly boosts earning potential in data analytics.
